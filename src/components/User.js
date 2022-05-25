9282e316f3c5e18b1949452040b7a3b599be684f
Aly Baez <alybaez@Alys-MBP.lan>
2022-05-25 14:14:54 -0700

    adds tenerary for updating api

--- src/components/User.js
+++ src/components/User.js
@@ -1,12 +1,31 @@
 import React from "react";
+import { useState } from "react";
 import "../styles/user.css";
+import editIcon from "../edit-icon.png";
+import checkIcon from "../icons-checked.png";
 
 function User({ id, name, email }) {
   const axios = require("axios");
 
+  const [updateEmail, setUpdateEmail] = useState("");
+  const [editOpen, setEditOpen] = useState(false);
+
   const handleDelete = (id) => {
     axios
       .delete(`http://localhost:3000/users/${id}/`)
+      .then((resp) => {
+        console.log(resp.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const handleUpdate = (id) => {
+    axios
+      .put(`http://localhost:3000/users/${id}/`, {
+        email: "freddyb34@yahoo.com",
+      })
       .then((resp) => {
         console.log(resp.data);
       })
@@ -21,7 +40,34 @@ function User({ id, name, email }) {
         X
       </div>
       <div className="padding">{name}</div>
-      <div className="padding email">{email}</div>
+      <div className="editWrapper">
+        {/* <div className="padding email" onClick={() => handleUpdate(id)}>
+          {email}
+        </div> */}
+        {editOpen ? (
+          <>
+            <input placeholder={email} />
+            <img
+              className="checked"
+              onClick={() => setEditOpen(!editOpen)}
+              src={checkIcon}
+              alt="Submit Updated Email"
+            />
+          </>
+        ) : (
+          <>
+            <div className="padding email" onClick={() => handleUpdate(id)}>
+              {email}
+            </div>
+            <img
+              className="edit"
+              onClick={() => setEditOpen(!editOpen)}
+              src={editIcon}
+              alt="Edit Email"
+            />
+          </>
+        )}
+      </div>
     </div>
   );
 }
